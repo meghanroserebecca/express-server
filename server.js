a925d6b349d12cf817dfe5553f3252f5958a1b61
meghanroserebecca <meghanroserebecca@gmail.com>
2017-02-06 15:27:37 -0800

    got server.js hooked up to mongodb and MongoClient; am now able to check out documents inside of the mystical-animals db

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
 const http = require('http');
 const fs = require('fs');
 
+const mongodb = require('mongodb');
+    //MongoClient is thing to connect to mongodb and be a "client"
+const MongoClient = mongodb.MongoClient
+
+const db_uri = 'mongodb://localhost:27017/mythical-animals';
+
+MongoClient.connect(db_uri, function(err, db) {
+    if(err) return console.log('FAIL!', err);
+
+    console.log('Connected correctly to server');
+
+    db.collection('unichr0ns').find().toArray()
+        .then(unichr0ns => console.log(unichr0ns))
+        .catch(err => console.log(err));
+});
+
+
+
 http.createServer((req, res) => {
-    fs.readFile('index.html', 'utf8', (err, contents) => {
-        res.end(contents);
-    });
+    
+    const stream = fs.createReadStream('index.html', { encoding: 'utf8' });
+
+    // let contents = '';
+    
+    // stream.on('data', data => {
+    //     res.write(data);
+    // });
+
+    // stream.on('end', () => {
+    //     res.end(contents);
+    // });
+
+    //the above can all be written as short-hand:
+
+    stream.pipe(res);
+    
+
+
 }).listen(3000);
